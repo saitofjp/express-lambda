ee2070276402ac4676260c610e01c272d7389f66
EC2 Default User <ec2-user@ip-172-31-23-192.ap-northeast-1.compute.internal>
2019-11-27 14:12:32 +0000

    nuxt3

--- server/index.js
+++ server/index.js
@@ -1,13 +1,14 @@
+const awsServerlessExpress = require('aws-serverless-express')
 const express = require('express')
-const consola = require('consola')
 const { Nuxt, Builder } = require('nuxt')
+const { customDomainAdaptorMiddleware } = require('./middleware');
 const app = express()
 
 // Import and Set Nuxt.js options
-const config = require('../nuxt.config.js')
-config.dev = process.env.NODE_ENV !== 'production'
+let config = require('../nuxt.config.js')
+config.dev = false
 
-async function start() {
+async function initApp() {
   // Init Nuxt.js
   const nuxt = new Nuxt(config)
 
@@ -22,13 +23,38 @@ async function start() {
   }
 
   // Give nuxt middleware to express
+  app.use(customDomainAdaptorMiddleware)
   app.use(nuxt.render)
 
-  // Listen the server
-  app.listen(port, host)
-  consola.ready({
-    message: `Server listening on http://${host}:${port}`,
-    badge: true
+  return app
+}
+
+var server = undefined;
+const binaryMimeTypes = [
+  'application/javascript',
+  'application/json',
+  'application/octet-stream',
+  'application/xml',
+  'font/eot',
+  'font/opentype',
+  'font/otf',
+  'image/jpeg',
+  'image/png',
+  'image/svg+xml',
+  'text/comma-separated-values',
+  'text/css',
+  'text/html',
+  'text/javascript',
+  'text/plain',
+  'text/text',
+  'text/xml'
+]
+
+exports.handler = (event, context) => {
+  initApp().then((app) => {
+    if (server === undefined) {
+      server = awsServerlessExpress.createServer(app, null, binaryMimeTypes)
+    }
+    awsServerlessExpress.proxy(server, event, context)
   })
 }
-start()
---
+++ server/middleware.js
@@ -0,0 +1,16 @@
+let config = require('../nuxt.config.js')
+
+const customDomainAdaptorMiddleware = (req, res, next) => {
+    const apigatewayHeader = req.headers['x-apigateway-event'];
+
+    if (apigatewayHeader === undefined) {
+      next()
+      return
+    }
+
+    req.url = req.originalUrl = `${config.router.base}${req.url}`.replace('//', '/')
+
+    next()
+};
+
+module.exports = {customDomainAdaptorMiddleware};
